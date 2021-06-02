c853fa4b5d9a6aea5f64567423673a4f22228f2c
Rodrigo Escobar <Rodesco89@gmail.com>
2021-06-01 21:18:09 -0400

    Static variables practices

---
+++ src/day49_static/StaticVariableTest.java
@@ -0,0 +1,11 @@
+package day49_static;
+
+public class StaticVariableTest {
+    public static void main(String[] args) {
+        System.out.println("name: " + StaticVariables.name);
+        System.out.println("userName: " + StaticVariables.userName);
+        System.out.println("age: " + StaticVariables.age);
+        System.out.println("price: " + StaticVariables.price);
+
+    }
+}
