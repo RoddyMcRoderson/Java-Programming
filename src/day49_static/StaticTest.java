3928fe3c3b4eafbd8d886dd7d97e0e94a8c74053
Rodrigo Escobar <Rodesco89@gmail.com>
2021-06-01 21:05:11 -0400

    finished static test class

--- src/day49_static/StaticTest.java
+++ src/day49_static/StaticTest.java
@@ -1,6 +1,7 @@
 package day49_static;
 
 public class StaticTest {
+    static String word = "java";
     public static void main(String[] args) {
         //1) static method can be called using classname
         StaticMethods.displayMessage("wooden spoon");
@@ -8,6 +9,9 @@ public class StaticTest {
         //StaticMethods.instanceMethod(); ERROR
         StaticMethods stm = new StaticMethods();
         stm.instanceMethod();
+
+        //2) static method can only access other static variables and methods
+        System.out.println(word);
 
     }
 }
