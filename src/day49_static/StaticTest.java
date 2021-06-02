39ab21a9e866d334e639cad5cd63f62244ba21df
Rodrigo Escobar <Rodesco89@gmail.com>
2021-06-01 20:12:44 -0400

    some work done in static test class

--- src/day49_static/StaticTest.java
+++ src/day49_static/StaticTest.java
@@ -1,4 +1,13 @@
 package day49_static;
 
 public class StaticTest {
+    public static void main(String[] args) {
+        //1) static method can be called using classname
+        StaticMethods.displayMessage("wooden spoon");
+        //1) instance method needs an object to be called
+        //StaticMethods.instanceMethod(); ERROR
+        StaticMethods stm = new StaticMethods();
+        stm.instanceMethod();
+
+    }
 }
