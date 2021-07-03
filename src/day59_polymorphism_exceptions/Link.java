878cedb009d2174b196bbc57a276443227fa9ac4
Rodrigo Escobar <Rodesco89@gmail.com>
2021-07-03 11:59:49 -0400

    day59 polymorphism_exceptions

---
+++ src/day59_polymorphism_exceptions/Link.java
@@ -0,0 +1,18 @@
+package day59_polymorphism_exceptions;
+
+public class Link implements WebElement{
+    @Override
+    public void sendKeys(String txt) {
+
+    }
+
+    @Override
+    public void click() {
+
+    }
+
+    @Override
+    public String getText() {
+        return null;
+    }
+}
