ba3189a76562a54a8abaf4e763493c395075dfb5
Rodrigo Escobar <Rodesco89@gmail.com>
2021-07-11 11:34:45 -0400

    throw keyword

---
+++ src/day61_exceptions_collections/ElementaryStudentTest.java
@@ -0,0 +1,11 @@
+package day61_exceptions_collections;
+
+public class ElementaryStudentTest {
+    public static void main(String[] args) {
+        ElementaryStudent el1 = new ElementaryStudent();
+        el1.setName("Bob");
+        el1.setAge(12);
+
+    }
+
+}
